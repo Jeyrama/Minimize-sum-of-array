6949797955a2707f145536db275085a8a943c0d8
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-03 18:38:27 -0700

    Update Minimize-sum-of-array.js

--- Minimize-sum-of-array.js
+++ Minimize-sum-of-array.js
@@ -12,3 +12,22 @@ minSum({5,4,2,3}) ==> return (22)
 
 
 // Solution
+
+function minSum(arr) {
+  return arr.sort((a,b) => a-b)
+            .slice(0, arr.length/2)
+            .reduce((acc, curr, index) => acc += curr * arr[arr.length - index - 1], 0)
+}
+
+// or
+
+function minSum(arr) {
+  let res = 0;
+  arr = arr.sort((a,b) => a-b);
+
+  while(arr.length) {
+    res += arr.pop()*arr.shift();
+  }
+
+  return res
+}
