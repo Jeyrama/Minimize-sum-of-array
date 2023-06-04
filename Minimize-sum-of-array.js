ec954c652fd34e8ad13d16f3edc74b737d7873af
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-03 18:23:01 -0700

    Create Minimize-sum-of-array.js

---
+++ Minimize-sum-of-array.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, find the minimum sum 
+which is obtained from summing each two integers' product.
+
+Notes:
+  Array/list will contain positives only.
+  Array/list will always have even size.
+
+Example:
+minSum({5,4,2,3}) ==> return (22) 
+*/
+
+
